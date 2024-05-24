0e439ec5913aec75c96e7ec9730f4734b303592f
Jeremiah <jeremiahjacobson10@gmail.com>
2024-05-23 21:52:31 -0400

    update backend schema to account for time zone for creation time

--- backend/schema.sql
+++ backend/schema.sql
@@ -17,12 +17,15 @@ CREATE DATABASE bugtracker
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    email VARCHAR(100) DEFAULT 'noemail',
+    email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
     role VARCHAR(20) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	reset_password_token VARCHAR(255),
-	reset_password_expires BIGINT
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    is_active BOOLEAN DEFAULT TRUE,
+    reset_password_token VARCHAR(255),
+    reset_password_expires BIGINT
 );
 
 -- Projects Table
@@ -30,7 +33,8 @@ CREATE TABLE projects (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) UNIQUE NOT NULL,
     description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    is_active BOOLEAN DEFAULT TRUE
 );
 
 -- Bugs Table with Full-Text Search and JSONB Metadata
@@ -40,12 +44,12 @@ CREATE TABLE bugs (
     description TEXT,
     status VARCHAR(20) NOT NULL,
     priority VARCHAR(20) NOT NULL,
-    project_id INT REFERENCES projects(id),
+    project_id INT REFERENCES projects(id) ON DELETE CASCADE,
     reported_by INT REFERENCES users(id),
-    assigned_to INT REFERENCES users(id),
+    assigned_to INT REFERENCES users(id) ON DELETE SET NULL,
     metadata JSONB,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Comments Table
@@ -54,21 +58,31 @@ CREATE TABLE comments (
     comment TEXT NOT NULL,
     bug_id INT REFERENCES bugs(id),
     user_id INT REFERENCES users(id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Bug History Table
 CREATE TABLE bug_history (
     id SERIAL PRIMARY KEY,
     bug_id INT REFERENCES bugs(id),
+    action_type VARCHAR(50),  -- e.g., 'update', 'comment', 'status change'
+    field_changed VARCHAR(50),
+    old_value TEXT,
+    new_value TEXT,
     status VARCHAR(20),
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_by INT REFERENCES users(id)
 );
+
+
 
 
 SELECT * FROM projects
 SELECT * FROM users
 SELECT * FROM bug_history
 SELECT * FROM bugs
+SELECT * FROM comments
+SELECT * FROM user_roles
+SELECT * FROM roles
+
 
