33b70b358443dacd21dcabce9f785b08f1981686
Jeremiah <jeremiahjacobson10@gmail.com>
2024-06-13 16:48:21 -0400

    Update styling to allow custom color palette

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
+
 import { AuthProvider } from "./contexts/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
